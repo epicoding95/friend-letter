22008cf25d876bbb30d5d2d83b2b05df9f02632a
Student <example@example.com>
2019-07-28 13:26:14 -0700

    added a few comments

--- FriendLetter.Solution/FriendLetter/Controllers/HomeController.cs
+++ FriendLetter.Solution/FriendLetter/Controllers/HomeController.cs
@@ -40,3 +40,17 @@ namespace FriendLetter.Controllers
     
   }
 }
+////MVC
+
+///Models-- model is data used by a program
+/// View-- a view is means of displaying objects within an aplication
+/// Controller-- a controller updates both models and views. it accepts input and performs the corresponding update.
+
+/// Razor-- Razor is a markup syntax that lets you embed server-based code
+
+
+/// what are the steps for setting up an ASP.NET MVC project?
+
+
+
+///How can we gather form data in an MVC route? What needs to be in place to make this possible? --- setting up the routes as we did above
